02a73207b591583d346948d6eeef67d683aaa8d8
ryoha000 <the7uya@gmail.com>
2022-06-02 00:23:33 +0900

    add echo in run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-g++ -std=c++17 -o test_lib test.cpp libkissfft-float.a
+echo "[INFO] build test start"
+g++ -std=c++17 -o test_lib test.cpp libkissfft-float.a -Ieigen -ILBFGSpp/include
+echo "[INFO] build test end"
+echo "[INFO] run test"
 ./test_lib
+echo "[INFO] end test"
